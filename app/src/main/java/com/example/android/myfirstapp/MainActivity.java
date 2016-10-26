5b1d687ae410a980b7088494b2454ff17fc8ad25
Android <mohitlivetech2016@gmail.com>
2016-10-26 15:37:19 +0530

    10/26/2016  --> 11:50

--- app/src/main/java/com/example/android/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/android/myfirstapp/MainActivity.java
@@ -2,14 +2,39 @@ package com.example.android.myfirstapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+      Button button;
+        TextView text;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.second_activity);
+
+        button=(Button)findViewById(R.id.button);
+        text=(TextView)findViewById(R.id.text1);
+
+
+        button.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+            text.setText(" Hello Sir :) ");
+
+            }
+
+
+        });
+
     }
+
+
 
 
 }
