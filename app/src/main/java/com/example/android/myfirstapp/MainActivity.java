a18ddc0f5e92755544b6b313ad74a41a0d6f025f
Android <mohitlivetech2016@gmail.com>
2016-11-01 14:37:10 +0530

    update 2:30 with proper convection

--- app/src/main/java/com/example/android/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/android/myfirstapp/MainActivity.java
@@ -11,61 +11,111 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 
+public class MainActivity extends AppCompatActivity {
+    public Button getmBtnTvDisplay() {
+        return mBtnTvDisplay;
+    }
 
-    public class MainActivity extends AppCompatActivity {
-        Button button,button1,button2;
-        TextView text;
-        EditText e1,e2;
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_main);
+    public void setmBtnTvDisplay(Button mBtnEvDisplay) {
+        this.mBtnTvDisplay = mBtnEvDisplay;
+    }
 
-            button=(Button)findViewById(R.id.button);
-            text=(TextView)findViewById(R.id.text1);
-            button1=(Button)findViewById(R.id.button1);
-            e1=(EditText)findViewById(R.id.text11);
-            e2=(EditText)findViewById(R.id.text22);
-            button2=(Button) findViewById(R.id.button2);
-            button.setOnClickListener(new View.OnClickListener()
-            {
-                @Override
-                public void onClick(View v)
-                {
-                    text.setText(" Hello Sir :) ");
+    public Button getmBtnNewLout() {
+        return mBtnNewLout;
+    }
 
-                }
+    public void setmBtnNewLout(Button mBtnNewLout) {
+        this.mBtnNewLout = mBtnNewLout;
+    }
 
+    public Button getmBtnCalc() {
+        return mBtnCalc;
+    }
 
-            });
-
-            button1.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-
-                    Intent intent=new Intent(MainActivity.this,Main2Activity.class);
-                    startActivity(intent);
-                }
-            });
-
-            button2.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-
-                    Intent intent=new Intent(MainActivity.this,Main5Activity.class);
-                    Bundle b = new Bundle();
-
-                    //Inserts a String value into the mapping of this Bundle
-                    b.putString("value1", e1.getText().toString());
-                    b.putString("value2", e2.getText().toString());
-                    intent.putExtras(b);
-                    startActivity(intent);
-
-                }
-            });
+    public void setmBtnCalc(Button mBtnCalc) {
+        this.mBtnCalc = mBtnCalc;
+    }
 
 
-        }
+    public EditText getmEvValue1() {
+        return mEtValue1;
+    }
+
+    public void setmEvValue1(EditText mEvValue1) {
+        this.mEtValue1 = mEvValue1;
+    }
+
+    public TextView getmTxtDisplay() {
+        return mTvDisplay;
+    }
+
+    public void setmTxtDisplay(TextView mTxtDisplay) {
+        this.mTvDisplay = mTxtDisplay;
+    }
+
+    public EditText getmEvValue2() {
+        return mEtValue2;
+    }
+
+    public void setmEvValue2(EditText mEvValue2) {
+        this.mEtValue2 = mEvValue2;
+    }
+
+    private EditText mEtValue1;
+    private EditText mEtValue2;
+    private TextView mTvDisplay;
+    private Button mBtnTvDisplay;
+    private Button mBtnNewLout;
+    private Button mBtnCalc;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mBtnTvDisplay = (Button) findViewById(R.id.idBtnTvShow);
+        mTvDisplay = (TextView) findViewById(R.id.idTvDisplay);
+        mBtnNewLout = (Button) findViewById(R.id.idBtnNewLout);
+        mEtValue1 = (EditText) findViewById(R.id.idEtValue1);
+        mEtValue2 = (EditText) findViewById(R.id.idEtValue2);
+        mBtnCalc = (Button) findViewById(R.id.idBtnCalc);
+        mBtnTvDisplay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mTvDisplay.setText(" Hello Sir :) ");
+
+            }
+
+
+        });
+
+        mBtnNewLout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(MainActivity.this, Main2Activity.class);
+                startActivity(intent);
+            }
+        });
+
+        mBtnCalc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(MainActivity.this, Main5Activity.class);
+                Bundle b = new Bundle();
+
+                //Inserts a String value into the mapping of this Bundle
+                b.putString("value1", mEtValue1.getText().toString());
+                b.putString("value2", mEtValue2.getText().toString());
+                intent.putExtras(b);
+                startActivity(intent);
+
+            }
+        });
+
+
+    }
 
 
 
@@ -89,7 +139,7 @@ import android.widget.Toast;
         });*/
 
 
-    }
+}
 
 
 
